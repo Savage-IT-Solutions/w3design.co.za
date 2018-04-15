5a1143b6ace9cc38261f67523b11e2799ff75d10
Schalk Barnard <savage-it@users.noreply.github.com>
2018-04-15 17:14:32 +0200

    git commit

---
+++ src/assets/js/w3design.js
@@ -0,0 +1,37 @@
+var Collapse = function ($) {
+  /**
+   * Constants
+   */
+  const NAME = 'collapse';
+  const VERSION = '1.0.0';
+  const DATA_KEY = 'collapse';
+  const EVENT_KEY = '.' + DATA_KEY;
+  const Default = {
+    toggle: true,
+    parent: ''
+  };
+  const DefaultType = {
+    toggle: 'boolean',
+    parent: '(string|element)'
+  };
+  const Event = {
+    SHOW: 'show' + EVENT_KEY,
+    HIDDEN: 'hidden' + EVENT_KEY
+  }
+  const ClassName = {
+    SHOW: 'show',
+    COLLAPSE: 'collapse'
+  }
+  const Selector = {
+    ACTIVES: 'show',
+    DATA_TOGGLE: '[data-toggle="collapse"]'
+  };
+
+  var Collapse = {
+    function() {
+      function Collapse(element, config) {
+        
+      }
+    }
+  }
+}
